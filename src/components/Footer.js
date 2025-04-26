16b6921be98c0ac0b2fc259ff80037f82d5d1e79
Maurice <mwasmorris777@gmail.com>
2025-04-26 20:39:03 +0300

    Updated the Footer styling.

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -5,11 +5,10 @@ function Footer({ loading, data }) {
   // We dont want do display anything on the first mount
   if (!loading && !data) return null;
   return (
-    <div className="font-mono m-4 p-4 shadow-md text-center text-sm md:text-lg">
-      <div>Now You Know Thy Weather</div>
+    <div className="font-mono m-4 py-2 space-y-2 shadow-md text-center text-sm md:text-lg bg-blue-400">
+      <div className="text-xs">Now you know thy weather!</div>
       <div>
-        Copyright <CopyrightIcon fontSize="small" color="success" /> devmwas
-        2023
+        Copyright <CopyrightIcon fontSize="small" /> Devmwas 2023
       </div>
     </div>
   );
